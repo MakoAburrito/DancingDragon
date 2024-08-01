40509b515fa8d1580467d9aa7d92c2e95dd1c549
Melissa De Jesus <Mdejesus1127@gmail.com>
2024-07-31 23:31:04 -0400

    SECOND commit

--- Scripts/script.js
+++ Scripts/script.js
@@ -8,3 +8,12 @@ toggleButton.addEventListener('click', () => {
         navbarLinks[i].classList.toggle('active');
     }
 })
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4){
+        counter = 1;
+    }
+}, 5000)
